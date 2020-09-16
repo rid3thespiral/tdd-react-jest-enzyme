89b4afeea979cd08e00e8439154c6af4a4332b92
rid3thespiral <davidebattaglia1993@gmail.com>
2020-09-16 23:37:28 +0200

    4 tests developed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { render } from 'enzyme';
+import React, {Component} from 'react';
+import PersonList from './PersonList';
 
-function App() {
-  return (
+class App extends Component{
+  state = { people: [] }
+  render() {
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PersonList/>
     </div>
   );
+  }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,27 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import App from './App';
+import { shallow } from 'enzyme'
+import PersonList from './PersonList'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  let appWrapper;
+
+  beforeAll(()=>{
+     appWrapper = shallow(<App/>);
+  });
+
+  it('renders a person list', () => {
+    const personList = appWrapper.find(PersonList);
+    expect(personList).toHaveLength(1);
+  });
+
+  it('has state', ()=>{
+    const appState = appWrapper.state();
+    expect(appState).not.toBeNull();
+  });
+
+  it('has a people property on state', ()=>{
+    const appState = appWrapper.state();
+    expect(appState.people).toBeDefined();
+  });
 });
